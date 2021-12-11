a52b8b50e848de1270560e5d61492e2b68c16b2d
dshutrin <53375751+dshutrin@users.noreply.github.com>
2021-12-11 11:35:46 +1000

    Update main.cpp

--- 3_semestr/Dima/lab12/main.cpp
+++ 3_semestr/Dima/lab12/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <sstream>
 #include <string>
 #include <list>
+#include <conio.h>
 using namespace std;
 
 template <class T>
@@ -35,15 +35,67 @@ public:
 	}
 };
 
-
 int main(){
-	Stack<int> st;
-	
-	st.add(1);
-	st.add(3);
-	st.add(2);
+	system("chcp 65001");
+	system("cls");
 
-	while (st.size > 0)
-		cout << st.get() << endl;
+	Stack<int> st;		int a;
+	Stack<double> st1;	double b;
+	Stack<string> st2;	string s;
 
+	int menu = 0;
+	while (menu != 4){
+		cout << "Выберите, с каким типом даннных вы хотите работать:\n1) int\n2) double\n3) string\n4) Exit" << endl;
+		cin >> menu;
+		cin.get();
+
+		if (menu == 1){
+			bool good = true;
+			cout << "Введите число (int): ";
+			while (!(cin >> a)){
+				cout << "error\n";
+        		cin.clear();      
+        		fflush(stdin);  
+			}
+			
+			st.add(a);
+		}
+
+		if (menu == 2){
+			bool good = true;
+			cout << "Введите число (double): ";
+			while (!(cin >> b)){
+				cout << "error\n";
+        		cin.clear();      
+        		fflush(stdin);  
+			}
+			st1.add(b);
+		}
+
+		if (menu == 3){
+			getline(cin, s);
+			while (cin.fail()){
+				cin.clear();
+				getline(cin, s);
+			};
+			st2.add(s);
+		}
+		system("cls");
+	}
+
+	cout << "INT: ";
+	while(st.size > 0){
+		cout << st.get() << " ";
+	}
+	cout << endl << "DOUBLE: ";
+
+	while (st1.size > 0){
+		cout << st1.get() << " ";
+	}
+	cout << endl << "STRING: ";
+
+	while (st2.size > 0){
+		cout << st2.get() << " ";
+	}
+	cout << endl;
 }
